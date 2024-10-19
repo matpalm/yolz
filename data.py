068476757013f08f8201a2eb6488d64e45ce282f
Mat Kelcey <matthew.kelcey@gmail.com>
2024-10-19 11:15:36 +1100

    sample objs_per_batch from |obj_ids|

--- data.py
+++ data.py
@@ -34,9 +34,11 @@ class ConstrastiveExamples(object):
         img_a = np.array(img)
         return img_a
 
-    def _anc_pos_generator(self, n):
-        for _ in range(n):
-            for obj_id in self.obj_ids:
+    def _anc_pos_generator(self, batch_size, objs_per_batch):
+        for _ in range(batch_size):
+            random.shuffle(self.obj_ids)
+            for obj_id in self.obj_ids[:objs_per_batch]:
+
                 anc_fname = self._random_example_for(obj_id)
                 anc_img_a = self._load_fname(anc_fname)
                 yield anc_img_a, self.label_str_to_idx[obj_id]
@@ -47,34 +49,41 @@ class ConstrastiveExamples(object):
                 pos_img_a = self._load_fname(pos_fname)
                 yield pos_img_a, self.label_str_to_idx[obj_id]
 
-    def dataset(self, batch_size):
-        # for x, y in self._anc_pos_generator():
-        #     print(x.shape, y)
-        # return
+    def dataset(self, batch_size, objs_per_batch):
+
+        if objs_per_batch > len(self.obj_ids):
+            raise Exception(f"not enough obj_ids ({len(self.obj_ids)}) to sample"
+                            f" objs_per_batch ({objs_per_batch})")
 
         ds = tf.data.Dataset.from_generator(
-            lambda: self._anc_pos_generator(batch_size),
+            lambda: self._anc_pos_generator(batch_size, objs_per_batch),
             output_signature=(
                 tf.TensorSpec(shape=(None, None, 3), dtype=tf.uint8),  # img
                 tf.TensorSpec(shape=(), dtype=tf.int64),               # label
             )
         )
 
-        # each sub batch is anc & pos for each obj
+        # each sub batch is anc & pos pair for each obj
         # select ith object pair as x.reshape(-1, 2)[i]
-        ds = ds.batch(2*len(self.obj_ids))
+        # ((2C, HW, HW, 3), (2C))
+        ds = ds.batch(2*objs_per_batch)
 
-        # batch again for actual batch
+        # batch again for _actual_ batch
+        # ((B, 2C, HW, HW, 3), (B, 2C))
         ds = ds.batch(batch_size)
+
         return ds
 
 
 if __name__ == '__main__':
     c_egs = ConstrastiveExamples(
         root_dir='data/reference_egs',
-        obj_ids=['061', '111', '000'],  # simple R, G, B examples
+        obj_ids=["061","135","182",  # x3 red
+                 "111","153","198",  # x3 green
+                 "000","017","019"], # x3 blue
     )
-    ds = c_egs.dataset(batch_size=5)
+    ds = c_egs.dataset(batch_size=5,
+                       objs_per_batch=3)
     for x, y in ds:
         print(x.shape, y)
 
